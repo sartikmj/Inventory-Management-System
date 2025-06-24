e2a85563a35ac7d5fe626379f2f893feaa4c3e9f
sartikmj <sartikmj@gmail.com>
2025-06-24 19:32:58 +0530

    database connected to the server

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+//importing all the dependencies
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express(); //initializing express app
+
+//setting up port
+const PORT = process.env.PORT || 5000;
+
+//connecting to the database ans start server
+//we are directly connecting this file to the database using mongoose
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(PORT, ()=>{
+             console.log(`Server is runnning on port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(`error connecting to the database: ${err}`);
+    })
+
+    //can also be done using async await
+//     async function startServer() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         app.listen(PORT, () => {
+//             console.log(`Server running on port ${PORT}`);
+//         });
+//     } catch (err) {
+//         console.error("Failed to connect to DB:", err);
+//     }
+// }
+// startServer();
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 cors -> help to manage request from frontend to backend
 dotenv -> helps to store our environment variables.
 body-parser -> parse req from frontend to the backend and access any info that is in the body.
+
+app.listen()
+->What it does:
+Starts the server
+
+Binds it to the given port (like 3000, 8080, etc.)
+
+Runs the callback once the server is up and listening
